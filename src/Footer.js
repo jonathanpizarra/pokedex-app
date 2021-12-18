ff444bb63e898d1aacf6fc7bd62ed0ecebd62670
Jonathan Pizarra <atanpizarra@gmail.com>
2021-12-18 14:52:39 +0800

    edited footer text

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ const Footer = ()=>{
     return(
         <div className='footer-container'>
             <p>My First React Project. It ain't much, but it's honest work XD</p>
-            <p><a href='https://github.com/jonathanpizarra' target='_blank'>Jonathan Pizarra</a> | 2021</p>
+            <p><a href='https://github.com/jonathanpizarra' rel='noreferrer' target='_blank'>Jonathan Pizarra</a> | 2021</p>
 
         </div>
     )
